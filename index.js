c302c02178987c1105a57bb73fcaf7cdb42d4a5c
ShinichiShi <supreeth2020@gmail.com>
2024-08-02 08:58:58 +0530

    add express

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const http = require('http')
 const fs = require('fs')
 const url = require('url')
+
 const pg404 = fs.readFileSync('404.html','utf-8')
 
 http
     .createServer((req,res)=>{ 
         const query = url.parse(req.url,true)
+
         let filename =''
         if(query.pathname==='/'){
             filename='.'+'/index.html'
@@ -13,6 +15,7 @@ http
         else {
             filename='.'+query.pathname
         }
+
         fs.readFile(filename,(err,data)=>{
             if(err){
                 res.writeHead(404,{'Content-Type':'text/html'})
@@ -27,4 +30,23 @@ http
     })
     .listen(3000)
 
-   
+// const express = require('express')
+// const app = express()
+// const path=require('path')
+// app.get('/',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'index.html'))
+// })
+// .get('/index.html',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'index.html'))
+// })
+// .get('/about.html',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'about.html'))
+// })
+// .get('/contact.html',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'contact.html'))
+// })
+// .get('/:file',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'404.html'))
+// })
+// .listen(3000,()=>{console.log('listening')})
+
